fb638516f79a35ab94cc1f1975651219a7f2b935
Ethan Tilley <1008464@student.sae.edu.au>
2019-03-12 15:33:09 +1000

    Removed unused HTML UI

--- bird.js
+++ bird.js
@@ -1,6 +1,5 @@
 class Bird {
-  constructor(brain)
-  {
+  constructor( brain ) {
 
     this.y = height / 2;
     this.x = 64;
@@ -12,69 +11,71 @@ class Bird {
     this.score = 0;
     this.fitness = 0;
 
-    if (brain) {
+    if ( brain ) {
       this.brain = brain.copy();
-    }else {
-      this.brain = new NeuralNetwork(5, 8, 2);
+    } else {
+      this.brain = new NeuralNetwork( 5, 8, 2 );
     }
   }
 
   show() {
-    fill(255);
-    ellipse(this.x, this.y, 32, 32);
+    fill( 255 );
+    ellipse( this.x, this.y, 32, 32 );
   }
 
   up() {
     this.velocity += this.lift;
   }
 
-  think(pipes) {
+  think( pipes ) {
 
     let closest = null;
     let closestD = Infinity;
-    for (let i = 0; i < pipes.length; i++) {
-      let d = (pipes[i].x + pipes[i].x) - this.x;
+    for ( let i = 0; i < pipes.length; i++ ) {
+      let d = ( pipes[ i ].x + pipes[ i ].x ) - this.x;
 
-      if(d < closestD && d > 0)
-      {
-        closest = pipes[i];
+      if ( d < closestD && d > 0 ) {
+        closest = pipes[ i ];
         closestD = d;
       }
     }
 
+    // Creating inputs and assigning some knowledge for the agent to use.
     let inputs = [];
-    inputs[0] = this.y / height;
-    inputs[1] = closest.top / height;
-    inputs[2] = closest.bottom / height;
-    inputs[3] = closest.x / width;
-    inputs[4] = this.velocity;
+    inputs[ 0 ] = this.y / height;
+    inputs[ 1 ] = closest.top / height;
+    inputs[ 2 ] = closest.bottom / height;
+    inputs[ 3 ] = closest.x / width;
+    inputs[ 4 ] = this.velocity;
 
+    // Creating the outputs and telling it to predict based on the known inputs
+    let output = this.brain.predict( inputs );
 
-
-    let output = this.brain.predict(inputs);
-    if (output[0] > output[1]) {
+    // If the agent things it should jump
+    if ( output[ 0 ] > output[ 1 ] ) {
+      // Then go for it
       this.up();
     }
   }
 
-  mutate()
-  {
-    this.brain.mutate(0.1);
+  /// Mutate this agents brain and give it a value for come leway for randomness (so it aint an exact copy)
+  mutate() {
+    this.brain.mutate( 0.1 );
   }
 
-  offScreen(){
-    return(this.y > height || this.y < 0);
+  /// Called to check if the agent is above or below the canvas area
+  offScreen() {
+    return ( this.y > height || this.y < 0 );
   }
 
 
 
   update() {
+    // Increase the agents score as a reward for surviving
     this.score++;
+    // Apply gravity to the agent
     this.velocity += this.gravity;
-    // this.velocity *= 0.9;
     this.y += this.velocity;
-
-
   }
 
 }
--- sketch.js
+++ sketch.js
@@ -5,78 +5,84 @@ let pipes = [];
 let counter = 0;
 let slider;
 
+// called on initialization of script
 function setup() {
-  createCanvas(640, 480);
-  slider = createSlider(1,100,1);
-  for (let i = 0; i < initPopulation; i++) {
-    birds[i] = new Bird();
+  createCanvas( 640, 480 );
+
+  // creating a slider to control the speed of the game (to quicken training)
+  slider = createSlider( 1, 100, 1 );
+
+  // Spawn the initial batch of agents
+  for ( let i = 0; i < initPopulation; i++ ) {
+    birds[ i ] = new Bird();
   }
 }
 
+// Called every frame
 function draw() {
 
-for (let n = 0; n < slider.value(); n++) {
+  // Quick for loop to speed up training.
+  for ( let n = 0; n < slider.value(); n++ ) {
 
-  if (counter % 75 == 0) {
-    pipes.push(new Pipe());
-  }
-  counter++;
+    // if time has passed, creat a new pipe.
+    if ( counter % 75 == 0 ) {
+      pipes.push( new Pipe() );
+    }
+    counter++;
 
-  for (var i = pipes.length-1; i >= 0; i--) {
-    pipes[i].update();
+    for ( var i = pipes.length - 1; i >= 0; i-- ) {
+      pipes[ i ].update();
 
-    for (let b = birds.length - 1; b >= 0; b--) {
-      if (pipes[i].hits(birds[b])) {
-        birdsBackUp.push(birds.splice(b,1)[0]);
+      for ( let b = birds.length - 1; b >= 0; b-- ) {
+        if ( pipes[ i ].hits( birds[ b ] ) ) {
+          birdsBackUp.push( birds.splice( b, 1 )[ 0 ] );
+
+        }
+      }
+
+
+      if ( pipes[ i ].offscreen() ) {
+        pipes.splice( i, 1 );
+      }
+    }
+
+    for ( let i = birds.length - 1; i >= 0; i-- ) {
+      if ( birds[ i ].offScreen() ) {
+        birdsBackUp.push( birds.splice( i, 1 )[ 0 ] );
 
       }
     }
 
-
-    if (pipes[i].offscreen()) {
-      pipes.splice(i, 1);
-    }
-  }
-
-  for (let i = birds.length - 1; i >= 0; i--) {
-    if (birds[i].offScreen()) {
-      birdsBackUp.push(birds.splice(i,1)[0]);
+    for ( let bird of birds ) {
+      bird.think( pipes );
+      bird.update();
 
     }
-  }
 
-  for (let bird of birds) {
-    bird.think(pipes);
-    bird.update();
-
+    if ( birds.length === 0 ) {
+      counter = 0;
+      pipes = [];
+      //pipes.push(new Pipe());
+      NextGeneration();
+    }
   }
-
-  if (birds.length === 0) {
-    counter = 0;
-    pipes = [];
-    //pipes.push(new Pipe());
-    NextGeneration();
-  }
-}
 
 
   // All the display
-  background(0);
+  background( 0 );
 
-  for(let bird of birds)
-  {
+  for ( let bird of birds ) {
     bird.show();
 
   }
-  for(let pipe of pipes)
-  {
+  for ( let pipe of pipes ) {
     pipe.show();
 
   }
 
 }
 
-  //
+//
 // function keyPressed() {
 //   if (key == ' ') {
 //     bird.up();
